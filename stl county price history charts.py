87f38bcf298dc1c038c7fa8a5536f320999483f3
Jwig99 <79022140+jolwig@users.noreply.github.com>
2023-12-14 13:38:11 -0600

    Update stl county price history charts.py

--- stl county price history charts.py
+++ stl county price history charts.py
@@ -1,25 +1,3 @@
-import pandas as pd
-import numpy as np
-import plotly.express as px
-
-#Import and clean data
-
-df = pd.read_csv(r"C:\Users\13148\Desktop\DataSets\zillow real estate.csv")
-#drop columns (only want to see st louis metro area)
-df = df.drop(columns=["RegionID","SizeRank", "RegionType","StateName", "State"])
-df = df.rename(columns={"RegionName" : "Zip Code", "CountyName" : "County"})
-df = df[df["Metro"] == "St. Louis"]
-df = df.sort_values(by="City")
-df = df.drop(columns=["Metro", "Zip Code","County"])
-#transpose df and reset index
-df = df.T.reset_index()
-df.rename(columns= df.iloc[0], inplace=True)
-df.rename(columns= {"City" : "Date"}, inplace=True)
-df.drop([0], inplace=True)
-
-for i in df:
-    fig = px.line(df, x=df["Date"], y=df[i])
-    fig.show()
 
 
 
